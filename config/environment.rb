4b1f7167c5954cb3eb57ad1a72628be3470acfa8
shantelraya <parischax@gmail.com>
2022-10-29 12:11:41 +0300

    done assignments

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,15 @@
 # Require statements should go here.
+require_relative '../lib/garden'
+require_relative '../lib/plant'
 
+lawn = Garden.new(name: 'Front Lawn')
+
+basil = Plant.new(name: 'Basil')
+basil.garden = lawn 
+
+cucumber = Plant.new(name: 'Cucumber')
+cucumber.garden = lawn
+
+p lawn.plants
 # Then any files that need to load the files required here can
 # require THIS file, environment.rb, and get access to everything
